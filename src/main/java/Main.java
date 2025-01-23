3c6bd5cf0cf68c1e8debb81a9981efcfad1f52ea
khainam23 <khainam23@gmail.com>
2025-01-23 17:20:03 +0700

    Create class connect database

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,4 +2,8 @@ public class Main {
     public static void main(String[] args) {
 
     }
+
+    /**
+     * Dùng để khởi chạy và so sánh cá thuật toán chung
+     */
 }
--- src/main/java/org/algorithm/Location.java
+++ src/main/java/org/algorithm/Location.java
@@ -1,32 +1,22 @@
 package org.algorithm;
 
 import lombok.AccessLevel;
-import lombok.Getter;
 import lombok.experimental.FieldDefaults;
 
-import java.awt.*;
-
-/**
- * Thể hiện tính chất của một điểm trong không gian
- */
 @FieldDefaults(level = AccessLevel.PRIVATE)
-@Getter
 public class Location {
-    Point point; // Tọa độ trong không gian
-    boolean isPickup; // Đã lấy hàng chưa
-    boolean isDrop; // Đã thả hàng chưa
-    int loadPickup; // Số lượng phải lấy
-    int loadDrop; // Số lượng phải thả
-    int LTW; // Low time window: Thời gian sớm nhất có thể tới
-    int UTW; // Upper time window: Thời gian trễ nhất có thể tới
+    long id;
+    long no;
+    int x;
+    int y;
+    int demandPick;
+    int demandDeliver;
+    int lowerTime;
+    int upperTime;
+    int serviceTime; // Bao gồm cả thời lấy và thời gian trả hàng
 
-    // Tính khoảng cách tọa độ (Euclid)
-    public double calculatorDistance(Location otherLocation) {
-        return this.point.distance(otherLocation.point);
-    }
-
-    // Số lượng dịch vụ phải làm tại điểm
-    public int calculatorService() {
-        return loadDrop + loadPickup;
-    }
+    /**
+     * Dựa vào các thuộc tính trong database của VRPTW mà triển khai
+     * các thuộc tính của một địa điểm
+     */
 }
